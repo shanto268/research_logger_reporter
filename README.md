596c721c17c73cfd6aa2a89ea64f4f2179a5c7e4
Sadman Ahmed Shanto <shanto@usc.edu>
2022-05-27 16:09:30 -0700

    added summary reporter functionality

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
 **WORKS ONLY FOR MAC OS. CAN BE EASILY CHANGED FOR LINUX BY USING CRON INSTEAD OF LAUNCHD**
 
+# Requirements
+-[] have `python` installed on your machine
+-[] have `pandoc` installed on your machine (*just `brew install pandoc`*)
 
 # Purpose
 
-Creates directory based on the day for tracking research progress (i.e. goals, summary and any work done)
+1. Creates directory based on the day for tracking research progress (i.e. goals, summary and any work done). 
+2. Creates a **summary pdf** based on all the markdown logs (an accumulation of) you have made each week at a certain time/day (i.e. change to sometime before your 1 to 1 meetings etc)
 
 # Configuration
 
-1. **Update the python code**: 
+## For Work Logging:
+
+1. **Update the `workLogger.py` code**: 
   - Change the project root file directory (i.e. `project_root` variable)
   - add any more files to be auto-generated (i.e. `files` variable)
 
-2. **Uupdate the `launchd` file:**
+2. **Update the `LFLworkLogger-launchd` file:**
   - rename the file to be more descriptive of your requirements
   - update the path to `python` binary
   - update the `text, title` variables to meet your alert needs
 
 
-3. **Uupdate the `plist` file:**
+3. **Update the `com.shanto.LFLworkLogger.plist` file:**
   - rename the file to be more descriptive of your requirements
     - make sure the new name is reflected on the `<string>` tag for the `Label <key>`
   - update the path to the launchd file (i.e. `<string> for the Program <key>`)
@@ -26,8 +32,52 @@ Creates directory based on the day for tracking research progress (i.e. goals, s
   - update the time when you want the daemon service to launch (*I have it set as 8 AM in the code*)
   - move the `plist` file to the `~/Library/LaunchAgents/` directory (create it if not already there)
 
+
+## For Summary Reporting:
+
+1. **Update the `createWeekSummaryReport.py` code**: 
+  - Change the following variables to match your needs
+    ```
+    project_root = "/Users/shanto/LFL/Summer22"
+    files = ["summary.md","goals.md"]
+    target_path = "/Users/shanto/LFL/Meetings/meeting_materials/summaries_of_the_week"
+    ```
+
+
+2. **Update the `LFLsummaryReporter-launchd` file:**
+  - rename the file to be more descriptive of your requirements
+  - update the path to `python` binary
+  - update the `text, title` variables to meet your alert needs
+
+
+3. **Update the `com.shanto.LFLsummaryReporter.plist` file:**
+  - rename the file to be more descriptive of your requirements
+    - make sure the new name is reflected on the `<string>` tag for the `Label <key>`
+  - update the path to the launchd file (i.e. `<string> for the Program <key>`)
+  - update all the `Standard*Paths`
+  - update the path to the code/local repo clone(i.e. `<string>` of `WorkingDirectory <key>`)
+  - update the time when you want the daemon service to launch (*mine is Tuesday 12 PM*) [**Weekday 7 == Sunday** and **Weekday 0 == Saturday**]
+
+    ```
+    <key>StartCalendarInterval</key>
+        <dict>
+            <key>Weekday</key>
+            <integer>2</integer>
+            <key>Hour</key>
+            <integer>12</integer>
+            <key>Minute</key>
+            <integer>0</integer>
+        </dict>
+      ```
+
+  - move the `plist` file to the `~/Library/LaunchAgents/` directory (create it if not already there)
+
+
 # Usage
 
 First test the code works as intended by running the `launchd` bash script. If it works well, then start the daemon process by
 
-`$ launchctl load -w nameOfPlistFile.plist #i.e. the .plist file in ~/Library/LaunchAgents/`
+`$ launchctl load -w nameOfWorkLoggerPlistFile.plist #i.e. the .plist file in ~/Library/LaunchAgents/`
+`$ launchctl load -w nameOfSummaryReporterPlistFile.plist #i.e. the .plist file in ~/Library/LaunchAgents/`
+
+**OF COURSE WORK LOGGER FUNCTIONALITY CAN BE USED WITHOUTH SUMMARY REPORTING**
---
+++ createWeekSummaryReport.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+=================================================
+Program : work_logger/createSummaryReport.py
+=================================================
+Summary:
+    - accumulates all the files from workLogger.py and creates pdf reports
+"""
+__author__ =  "Sadman Ahmed Shanto"
+__date__ = "05/27/2022"
+
+
+#libraries used
+import numpy as np
+import os, sys, datetime, glob, subprocess
+
+def get_all_paths(base_path):
+    global today
+    all_paths = []
+    # record today's date
+    today = str(datetime.date.today()).replace("-","_")
+    # find the path corresponding to today's dir
+    path = base_path + "/" + today
+    # calculate and find all the paths until the last 7 days
+    for day in range(8):
+        last_week_path = base_path + "/" + str(datetime.date.today() - datetime.timedelta(days=day)).replace("-","_")
+        all_paths.append(last_week_path)
+
+    # print("all_paths : {}".format(all_paths))
+    return all_paths
+
+def get_all_files(all_paths, files):
+    all_files = []
+    for path in all_paths:
+        for file in files:
+        #make sure the path exists
+            try:
+                # add all the required files to a list
+                all_files.append(path+"/"+file)
+            except:
+                pass
+    # sort the list by file name type
+    all_files.sort(key = lambda x: x.split("/")[-1])
+    # restructure the list based on the number of files
+    all_files = np.array(all_files)
+    all_files = np.array_split(all_files, len(files))
+    return all_files
+
+def create_file_dump(all_files, target_path):
+    fnames = []
+    line = "-"*3
+    end_line = "\n"+line+"\n\n"
+
+    for files in all_files:
+        i = 0 # for single shot loop execution
+        content = []
+        for file in files:
+            #creates the summary files with the correct name in the right directory
+            while i < 1: #this loop executes once
+                actual_file = file.split("/")[-1]
+                fname = target_path + "/" + today + "_" + actual_file
+                fnames.append(fname)
+                try:
+                    bashCommand = "touch {}".format(fname)
+                    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                    output, error = process.communicate()
+                except:
+                    pass
+                i += 1 # for single shot loop execution
+            try:
+                # read the present file and add it to memory
+                f_open = open(file, "r")
+                content.append(f_open.read())
+            except:
+                pass
+
+        print(fname + " has been created!")
+
+        # parse the file content
+        content = end_line.join(content)
+        content = content.replace("-[x]","\n\n-[x]")
+        content = content.replace("-[]","\n\n-[]")
+
+        # dump file content from memory to summary file
+        f_summary = open(fname, "a")
+        title = actual_file.split(".")[0].capitalize()
+        header = "---\ntitle: {}\nauthor: {}\ndate: {}\ngeometry: margin=2cm\n---\n".format(title,__author__,today.replace("_","-"))
+        f_summary.write(header)
+        f_summary.write(content)
+        f_summary.close()
+
+        #make pdf from markdown
+        try:
+            pdfname = fname.split(".")[0] + ".pdf"
+            bashCommand = "pandoc -s -o {} {}".format(pdfname,fname)
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+        except:
+            pass
+        print(pdfname + " has been created!")
+
+
+
+if __name__ == "__main__":
+    project_root = "/Users/shanto/LFL/Summer22"
+    files = ["summary.md","goals.md"]
+    target_path = "/Users/shanto/LFL/Meetings/meeting_materials/summaries_of_the_week"
+
+    all_paths = get_all_paths(project_root)
+    all_files = get_all_files(all_paths, files)
+    create_file_dump(all_files, target_path)
